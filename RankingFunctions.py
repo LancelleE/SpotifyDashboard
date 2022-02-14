99b055d0ba2d56f1b8eb2a12f97f66cd176e9aaf
etiennelancelle <elancelle.pro@gmail.com>
2022-02-14 23:37:49 +0100

    2e MAJ

--- RankingFunctions.py
+++ RankingFunctions.py
@@ -2,57 +2,15 @@
 """
 Created on Sun Feb 14 20:28:21 2021
 
-@author: etien
+@author: etienne
 """
 
-"""
-The aim of this script is to create functions that will help us to create rankings.
-
-"""
 from datetime import datetime, timedelta
-import plotly.graph_objects as go
-import plotly.express as px
-import glob
-import json
 import pandas as pd
-import pickle
-
-"""
-LIBRARY UPLOAD
-"""
-PATH = "DATA/my_spotify_data/MyData/"
-with open(PATH + 'YourLibrary.json', 'rb') as f:
-    lib = json.load(f)
-
-library = pd.DataFrame(lib['tracks'])
-
-library.rename(index=str, columns={
-    "artist": "Artist",
-    "album": "Album",
-    "track": "Track"
-}, inplace=True)
-
-"""
-STREAMINGHISTORY UPLOAD
-"""
-df_final = pickle.load(open("DATA/df_final.p", "rb"))
-
-"""
-MERGE TO GET ALBUMS
-"""
-
-full_data = pd.merge(df_final, library, how="left", on=['Track', 'Artist'])
-full_data['Date'] = pd.to_datetime(full_data['Date'], format='%Y-%m-%d %H:%M')
-full_data.insert(value=full_data['Date'].dt.hour, loc=5, column="Hour")
-
-"""
-
-j'ai fait les modifs dans mon spotify donc ce sera bon pour le prochain upload.
-
-"""
 
 
 def filter_hour(df, h_min, h_max):
+    df_filter = None
     if h_min < h_max:
         df_filter = df[(df.Hour >= h_min) & (df.Hour < h_max)]
     elif h_min == h_max:
@@ -61,28 +19,28 @@ def filter_hour(df, h_min, h_max):
         df_part1 = df[(df.Hour >= h_min) & (df.Hour < 24)]
         df_part2 = df[(df.Hour >= 0) & (df.Hour < h_max)]
         df_filter = pd.concat([df_part1, df_part2], axis=0)
-    return (df_filter)
+    return df_filter
 
 
 def filter_date(df, date_min, date_max):
     mini = min(date_min, date_max)
     maxi = max(date_min, date_max)
     df_filter = df[(df.Date >= mini) & (df.Date < maxi)]
-    return (df_filter)
+    return df_filter
 
 
 def seconds_to_format(value):
     sec = timedelta(seconds=value)
     d = datetime(1, 1, 1) + sec
     chain = "%dj %dh %dm %ds" % (d.day - 1, d.hour, d.minute, d.second)
-    return (chain)
+    return chain
 
 
 def seconds_to_format2(value):
     sec = timedelta(seconds=value)
     d = datetime(1, 1, 1) + sec
     chain = "%dh %dm %ds" % (d.hour, d.minute, d.second)
-    return (chain)
+    return chain
 
 
 """
@@ -99,10 +57,7 @@ def artiste_ranking(df, h_min, h_max, date_min, date_max):
 
     final_rank["Duration"] = final_rank["Duration"].apply(seconds_to_format)
     final_rank.insert(loc=0, column="Rank", value=range(1, 21))
-    return (final_rank)
-
-
-artiste_ranking(full_data, 0, 23, '2019-01-01', '2022-08-16')
+    return final_rank
 
 
 def album_ranking(df, h_min, h_max, date_min, date_max):
@@ -114,10 +69,7 @@ def album_ranking(df, h_min, h_max, date_min, date_max):
 
     final_rank["Duration"] = final_rank["Duration"].apply(seconds_to_format)
     final_rank.insert(loc=0, column="Rank", value=range(1, 21))
-    return (final_rank)
-
-
-album_ranking(full_data, 0, 23, '2019-01-01', '2022-08-16')
+    return final_rank
 
 
 def song_ranking(df, h_min, h_max, date_min, date_max):
@@ -129,29 +81,7 @@ def song_ranking(df, h_min, h_max, date_min, date_max):
 
     final_rank["Duration"] = final_rank["Duration"].apply(seconds_to_format)
     final_rank.insert(loc=0, column="Rank", value=range(1, 21))
-    return (final_rank)
-
-
-song_ranking(full_data, 0, 23, '2019-01-01', '2022-08-16')
-
-"""
-FOCUS ON AN ARTIST
-"""
-
-
-def artist_song_ranking(df, artist, h_min, h_max, date_min, date_max):
-    df_filter = filter_hour(df=df, h_min=h_min, h_max=h_max)
-    df_final_filtered = filter_date(df_filter, date_min=date_min, date_max=date_max)
-    df_artist = df_final_filtered[df_final_filtered["Artist"] == artist]
-
-    new_df = df_artist.groupby(['Track', 'Album'], sort=True)["Duration"].sum().reset_index()
-    final_rank = new_df.sort_values(by=['Duration'], ascending=[False])[0:20]
-
-    final_rank["Duration"] = final_rank["Duration"].apply(seconds_to_format2)
-    return (final_rank)
-
-
-# artist_song_ranking(full_data,"Niska",0,23,'2019-01-01','2022-08-16')
+    return final_rank
 
 
 def artist_song_ranking(df, artist, h_min, h_max, date_min, date_max):
@@ -163,10 +93,7 @@ def artist_song_ranking(df, artist, h_min, h_max, date_min, date_max):
     final_rank = new_df.sort_values(by=['Duration'], ascending=[False])[0:20]
 
     final_rank["Duration"] = final_rank["Duration"].apply(seconds_to_format2)
-    return (final_rank)
-
-
-# artist_song_ranking(full_data,"Angèle",0,23,'2019-01-01','2022-08-16')
+    return final_rank
 
 
 def artist_through_time(df, artist, h_min, h_max, date_min, date_max):
@@ -186,7 +113,7 @@ def artist_through_time(df, artist, h_min, h_max, date_min, date_max):
 
     final_table.fillna(0, inplace=True)
     final_table["DurationStyle"] = final_table["Duration"].apply(seconds_to_format2)
-    return (final_table)
+    return final_table
 
 
 def clock_graph_dataset(df, date_min, date_max):
@@ -195,31 +122,4 @@ def clock_graph_dataset(df, date_min, date_max):
     new_df = df_final.groupby(['Hour'])["Duration"].sum().reset_index()
     new_df["DurationStyle"] = new_df["Duration"].apply(seconds_to_format2)
     new_df["Hour"] = new_df["Hour"].astype(str)
-    return (new_df)
-
-
-test = clock_graph_dataset(full_data, '01-01-2020', '31-12-2020')
-
-fig = go.Figure(go.Barpolar(
-    r=test.Duration,
-    theta=test.Hour,
-    marker_line_color="black",
-    marker_line_width=2,
-    opacity=0.85,
-    hovertemplate="<b>%{theta}h</b><br><i>%{text}</i><extra></extra>",
-    text=test.DurationStyle.astype(str)
-))
-
-fig.write_html('first_figure.html', auto_open=True)
-
-# test = artist_through_time(full_data,"Liam Gallagher",0,24,'2019-10-01','2021-02-01')
-
-# fig = go.Figure(go.Bar(
-#     x = test.Date,
-#     y = test.Duration,
-#     hovertemplate =
-#     "<b>%{x}</b><br><i>%{text}</i><extra></extra>",
-#     text = test.DurationStyle.astype(str),
-#     showlegend = False))
-# fig.write_html('first_figure.html', auto_open=True)
-
+    return new_df
--- RecupHistory.py
+++ RecupHistory.py
@@ -1,32 +1,57 @@
 import pandas as pd
 import json
-import pickle
+import glob
 
 """
 LIBRARY UPLOAD
 """
 
 PATH = "DATA/my_spotify_data/MyData/"
-with open(PATH + 'YourLibrary.json', 'rb') as f:
-    lib = json.load(f)
 
-library = pd.DataFrame(lib['tracks'])
 
-library.rename(index=str, columns={
-    "artist": "Artist",
-    "album": "Album",
-    "track": "Track"
-}, inplace=True)
+def import_streaming_history(filepath):
+    with open(filepath, 'rb') as file:
+        data = json.load(file)
+    df = pd.DataFrame(data)
+    df['endTime'] = pd.to_datetime(df['endTime']).dt.strftime('%Y-%m-%d %i:%m')
+    df['msPlayed'] = df['msPlayed'] / 1000
+    return df
 
-"""
-STREAMINGHISTORY UPLOAD
-"""
-df_final = pickle.load(open("DATA/df_final.p", "rb"))
 
-"""
-MERGE TO GET ALBUMS
-"""
+def import_all_streaming_history_files():
+    file_list = glob.glob(PATH + "StreamingHistory**.json")
+    doc_list = []
+    for i in file_list:
+        doc_list.append(import_streaming_history(i))
 
-full_data = pd.merge(df_final, library, how="left", on=['Track', 'Artist'])
-full_data['Date'] = pd.to_datetime(full_data['Date'], format='%Y-%m-%d %H:%M')
-full_data.insert(value=full_data['Date'].dt.hour, loc=5, column="Hour")
+    full_df = pd.concat(doc_list, axis=0)
+
+    full_df.rename(index=str, columns={
+        "endTime": "Date",
+        "artistName": "Artist",
+        "trackName": "Track",
+        "msPlayed": "Duration"
+    }, inplace=True)
+
+    return full_df
+
+
+def import_library():
+    with open(PATH + 'YourLibrary.json', 'rb') as f:
+        lib = json.load(f)
+
+    library = pd.DataFrame(lib['tracks'])
+
+    library.rename(index=str, columns={
+        "artist": "Artist",
+        "album": "Album",
+        "track": "Track"
+    }, inplace=True)
+
+    return library
+
+
+def import_all():
+    streaming_history = import_all_streaming_history_files()
+    library = import_library
+    return streaming_history, library
