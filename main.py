ae562c971aaa6a2ccfa982eb4f1a48b18bbe7adb
etiennelancelle <elancelle.pro@gmail.com>
2022-02-15 00:21:41 +0100

    2e MAJ

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from SCRIPTS.DataScripts.RecupHistory import import_all
-
+from dash import Dash
+import brotli
 
 def main():
     streaming_history, library = import_all()
+    app = Dash(__name__)
 
 
 if __name__ == '__main__':
